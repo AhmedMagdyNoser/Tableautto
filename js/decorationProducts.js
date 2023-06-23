eeb2feb9bd3b674cc6c40c1621b54e7a1656dcfe
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-06-23 08:42:03 +0300

    Art and Decoration Pages Updated

--- js/decorationProducts.js
+++ js/decorationProducts.js
@@ -3,27 +3,27 @@ let landing = document.querySelector(".decoration-landing");
 let landingImageNum = 2;
 
 setInterval(() => {
-    landing.style.backgroundImage = `url(imgs/decoration/landing${landingImageNum}.jpg)`;
-    landing.style.backgroundSize = "cover";
-    (landingImageNum < 3) ? landingImageNum++ : landingImageNum = 1;
-}, 5000);
+  landing.style.backgroundImage = `url(imgs/decoration/landing${landingImageNum}.jpg)`;
+  (landingImageNum < 3) ? landingImageNum++ : landingImageNum = 1;
+}, 5000); 
 
 let cards = document.querySelectorAll(".products .container .card");
 
 // change background (overlay div) for each card
 cards.forEach(element => {
-    element.firstElementChild.style.backgroundImage = `url(${element.firstElementChild.nextElementSibling.getAttribute("src")})`;
+  element.firstElementChild.style.backgroundImage = `url(${element.firstElementChild.nextElementSibling.getAttribute("src")})`;
 })
 
 // show cards on scroll 
 showObjectsIfOnScreen(cards)
 window.addEventListener("scroll", () => {
-    showObjectsIfOnScreen(cards)
+  showObjectsIfOnScreen(cards)
 });
+
 function showObjectsIfOnScreen(objects) { // objects is an array of HTML elements
-    objects.forEach(element => {
-        if (window.pageYOffset + window.innerHeight > element.offsetTop) { // when scrolling into an element
-                element.style.opacity = "1";     
-        }
-    });
+  objects.forEach(element => {
+    if (window.pageYOffset + window.innerHeight > element.offsetTop) { // when scrolling into an element
+      element.style.opacity = "1";
+    }
+  });
 }
