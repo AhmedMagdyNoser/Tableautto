089795432acc31c04fc8ef19cd6e9570fa81db94
AhmedMagdyNoser <fi.d2i2h@gmail.com>
2023-07-23 13:00:01 +0300

    Page Loading Animation

--- js/global.js
+++ js/global.js
@@ -1,23 +1,38 @@
+// Loading Animation
+let loadingElement = `<div class="loading">
+                        <div class="custom-loader"></div>
+                      </div>`;
+
+document.body.innerHTML += loadingElement;
+
+let loading = document.querySelector(".loading");
+let page = document.querySelector(".page");
+
+window.addEventListener("load", () => {
+  loading.style.display = "none";
+  page.style.display = "block";
+});
+
 // Prevent the context menu
-document.addEventListener("contextmenu", e => {
-    e.preventDefault();
-})
+document.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+});
 
 // Header
 let toggleButton = document.getElementById("toggle-menu");
 toggleButton.onclick = () => {
-    document.querySelector("header .container nav").classList.toggle("opened");
-}
+  document.querySelector("header .container nav").classList.toggle("opened");
+};
 
 // Hide the header when scrolling down & Showing it when scrolling up
 let lastScrollTop = 0;
 let header = document.getElementById("header");
 window.addEventListener("scroll", () => {
-    let scrollTop = window.pageYOffset;
-    if (scrollTop > lastScrollTop) {
-        header.style.top = `-65px`;
-    } else {
-        header.style.top = "0";
-    }
-    lastScrollTop = scrollTop;
-})
+  let scrollTop = window.pageYOffset;
+  if (scrollTop > lastScrollTop) {
+    header.style.top = `-65px`;
+  } else {
+    header.style.top = "0";
+  }
+  lastScrollTop = scrollTop;
+});
